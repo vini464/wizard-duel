ec2a7fe1162d60595bb951965b07b42801f50972
vini464 <vini.dev2003@gmail.com>
2025-09-08 09:18:56 -0300

    feat(share): create shared structs

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vini464/wizard-duel
+
+go 1.23.3
---
+++ share/structs.go
@@ -0,0 +1,25 @@
+package share
+
+// this package only have structs type for server and client usage
+
+type User struct {
+	Username   string            `json:"username"`
+	Password   string            `json:"password"`
+	Collection map[string]int    `json:"collection"` // card name -> qnt
+	Cards      map[string]Card   `json:"card"`       // card name -> Card
+	Decks      map[string][]Card `json:"decks"`      // deck name -> card list
+	Coins      int               `json:"coins"`
+}
+
+type Card struct {
+	Name    string   `json:"name"`
+	Type    string   `json:"type"`
+	Rarity  string   `json:"rarity"`
+	Cost    int      `json:"cost"`
+	Effects []Effect `json:"effects"`
+}
+
+type Effect struct {
+	Type   string `json:"type"`
+	Amount int    `json:"amount"`
+}
