97112ed7a78eb11dffa4133d5b0a298f15c5b033
vini464 <vini.dev2003@gmail.com>
2025-09-09 10:02:59 -0300

    feat(persistence): add retrieve all cards function

--- persistence/card_persistence.go
+++ persistence/card_persistence.go
@@ -13,7 +13,7 @@ func SaveCard(filepath string, card share.Card) bool {
 		return false
 	}
 	var cards []share.Card
-	err = json.Unmarshal(f_bytes, &card)
+	err = json.Unmarshal(f_bytes, &cards)
 	if err != nil {
 		return false
 	}
@@ -80,4 +80,14 @@ func UpdateCard(filepath string, old_card share.Card, new_card share.Card) bool 
     return ok
   }
   return false
+}
+
+func RetrieveAllCards(filepath string) []share.Card {
+  cards := make([]share.Card, 0)
+	f_bytes, err := ReadFile(filepath)
+	if err != nil {
+		return cards
+	}
+	json.Unmarshal(f_bytes, &cards)
+  return cards
 }
