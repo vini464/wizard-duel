720043fce4e3993bafff3e517254c48b719e288a
vini464 <vini.dev2003@gmail.com>
2025-09-09 07:41:54 -0300

    refactor(*): deleting anything related to database, i'll use json instead

--- share/structs.go
+++ share/structs.go
@@ -3,12 +3,11 @@ package share
 // this package only have structs type for server and client usage
 
 type User struct {
-	Username   string            `json:"username"`
-	Password   string            `json:"password"`
-	Collection map[string]int    `json:"collection"` // card name -> qnt
-	Cards      map[string]Card   `json:"card"`       // card name -> Card
-	Decks      map[string][]Card `json:"decks"`      // deck name -> card list
-	Coins      int               `json:"coins"`
+	Username string            `json:"username"`
+	Password string            `json:"password"`
+	Cards    []Card            `json:"cards"`  
+	Decks    map[string][]Card `json:"decks"` // deck name -> card list
+	Coins    int               `json:"coins"`
 }
 
 type Card struct {
@@ -24,10 +23,10 @@ type Effect struct {
 	Amount int    `json:"amount"`
 }
 
-func NewUser(username string, password string) *User{
-  return &User{
-    Username: username,
-    Password: password, // hash it later
-    Coins: 120,
-  }
+func NewUser(username string, password string) *User {
+	return &User{
+		Username: username,
+		Password: password, // hash it later
+		Coins:    120,
+	}
 }
